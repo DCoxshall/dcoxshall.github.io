4c911ac84b90cfdd199f5d64643f2a50360f43f1
Dylan Coxshall <dacoxshall@gmail.com>
2022-10-20 18:19:10 +0100

    cleanup

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,7 +2,9 @@ import React from "react";
 
 class MainContent extends React.Component {
 	render() {
-		return <div id="mainContent">hi</div>;
+		return <div id="mainContent">
+			<h1 id="voteDylan">VOTE DYLAN FOR FIRST YEAR REP!</h1>
+		</div>;
 	}
 }
 
