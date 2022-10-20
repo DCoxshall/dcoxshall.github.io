88384cf72da04ec286a146d1636c02ed715d54fc
Dylan Coxshall <dacoxshall@gmail.com>
2022-10-20 18:05:26 +0100

    altered title

---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MainContent extends React.Component {
+	render() {
+		return <div id="mainContent">hi</div>;
+	}
+}
+
+export default MainContent;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Sidebar extends React.Component {
+	render() {
+		return (
+			<div id="sidebarContainer">
+				<div id="picContainer"></div>
+				<p>Dylan Coxshall</p>
+				<p>University of Birmingham</p>
+				<p>BSc Computer Science with Year in Industry</p>
+				<hr></hr>
+				<p>07956 084982</p>
+				<a href="mailto:dacoxshall@gmail.com">dacoxshall@gmail.com</a>
+				<p></p>
+			</div>
+		);
+	}
+}
+
+export default Sidebar;
